4a6e7c1973c36ce0f6b2b8eb8b4bb62a9e6c18f5
LuzkalidGM <gg2022074263@virtual.upt.pe>
2025-06-02 13:31:01 -0500

    primera parte

---
+++ PrimeService/Primes.Lib/PrimeService.cs
@@ -0,0 +1,11 @@
+namespace Primes.Lib
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate < 2) return false;
+            throw new NotImplementedException("Not implemented.");
+        }
+    }
+}
